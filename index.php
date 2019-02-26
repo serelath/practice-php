2bd360aa3e860780069a9d2a9492e96a46715866
Alice <shoh@go.pasadena.edu>
2019-02-25 17:08:02 -0800

    Variables and bool lesson

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
 
-echo "Hello Word";
+$name = "Alice";
+
+echo "<p>My name is $name</p";
+
+$string1 = "<p>This is the first part";
+
+$string2 = "of a sentence<p>";
+
+echo $string1." ".$string2;
+
+$myNumber = 13;
+
+$calculation = $myNumber * 31 / 97 + 4;
+
+echo "The result of the calculation is ".$calculation;
+
+$myBool = true;
+
+echo "<p>This statement is true? ".$myBool."</p>";
+
+$variableName = "name";
+
+echo $$variableName;
 
 ?>
